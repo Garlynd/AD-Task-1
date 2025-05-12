44bbd5f2fa36c7291348e6fd4440adc6a6084c49
Garlynd <weinard21@gmail.com>
2025-05-13 01:32:07 +0800

    feat(nav): add navigation component with links to Home, Gallery, and About

---
+++ components/nav.component.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/AD-Task-1/index.php">Home</a> |
+    <a href="/AD-Task-1/page/gallery/index.php">Gallery</a> |
+    <a href="/AD-Task-1/page/about/index.php">About</a>
+</nav>
+<hr>
