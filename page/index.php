5b1775b13ff5e185425bdf825676399a309f9c76
Garlynd <weinard21@gmail.com>
2025-05-13 01:43:17 +0800

    feat(gallery): update title and add CSS links for Gallery page

--- page/index.php
+++ page/index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gallery</title>
+    <title>Gallery - Cycling Team</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/gallery.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap');
+    </style>
 </head>
+
 <body>
-    
 </body>
 </html>
