5843f6397e7a1fcfe0a10fc1b807f57657923308
Garlynd <weinard21@gmail.com>
2025-05-13 00:42:21 +0800

    feat(home): display team members and bikes using PHP arrays

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$teamMembers = [
+    ["name" => "Mark", "image" => "assets/img/Mark.jpg"],
+    ["name" => "Peter", "image" => "assets/img/Peter.jpg"],
+    ["name" => "Lance", "image" => "assets/img/Lance.webp"],
+    ["name" => "Julian", "image" => "assets/img/Julian.jpg"]
+];
+
+$bikes = [
+    ["model" => "Specialized Tarmac SL7", "image" => "assets/img/tarmac.jpg"],
+    ["model" => "Canyon Aeroad CF SLX", "image" => "assets/img/aeroad.jpg"],
+    ["model" => "Trek Émonda SLR", "image" => "assets/img/emonda.webp"],
+    ["model" => "Giant Propel Advanced", "image" => "assets/img/propel.jpg"]
+];
+?>
