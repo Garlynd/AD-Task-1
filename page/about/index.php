be421109d2e10011dd0b61fffb47e2342817d9a2
Garlynd <weinard21@gmail.com>
2025-05-13 12:33:07 +0800

    refactor(about): update stylesheet path to use the correct directory structure

--- page/about/index.php
+++ page/about/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Our Cycling Team</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="../../assets/css/about.css">
+    <link rel="stylesheet" href="../../page/assets/css/about.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap');
     </style>
