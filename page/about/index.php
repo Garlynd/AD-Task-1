4741c78b4cb6fe9738aa3a751b5db0eb781afaf6
Garlynd <weinard21@gmail.com>
2025-05-13 10:18:27 +0800

    feat(about): create initial about page structure with HTML and linked styles

---
+++ page/about/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Our Cycling Team</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/about.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap');
+    </style>
+</head>
+
+<body>
+    
+</body>
+</html>
